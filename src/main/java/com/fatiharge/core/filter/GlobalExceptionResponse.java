ea0246982a2dba8e26963c63c816890ccc2f9ce3
fatihcetin <fatihcetin@tamfinans.com.tr>
2025-05-10 00:21:56 +0300

    refactor : traceId rm

--- src/main/java/com/fatiharge/core/filter/GlobalExceptionResponse.java
+++ src/main/java/com/fatiharge/core/filter/GlobalExceptionResponse.java
@@ -1,12 +1,12 @@
 package com.fatiharge.core.filter;
 
-public record GlobalExceptionResponse(String message, String reference, String exception, String traceId) {
+public record GlobalExceptionResponse(String message, String reference, String exception
+) {
     public static GlobalExceptionResponse fromException(Exception exception) {
         return new GlobalExceptionResponse(
                 exception.getMessage( ) == null ? exception.getClass( ).getSimpleName( ) : exception.getMessage( ),
                 exception.getClass( ).getName( ),
-                exception.getClass( ).getSimpleName( ),
-                "Trace Not Implemented"
+                exception.getClass( ).getSimpleName( )
         );
     }
 }
